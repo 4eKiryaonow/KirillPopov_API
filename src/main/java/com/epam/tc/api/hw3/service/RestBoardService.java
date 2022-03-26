d8e6c10b0b1b6b573500579dd607251e732ae09b
Kirill Popov <Kirill_Popov@epam.com>
2022-03-26 20:38:33 +0300

    create dto, common service, boardservice classes

--- src/main/java/com/epam/tc/api/hw3/service/RestBoardService.java
+++ src/main/java/com/epam/tc/api/hw3/service/RestBoardService.java
@@ -1,6 +1,7 @@
 package com.epam.tc.api.hw3.service;
 
 import static com.epam.tc.api.hw3.utils.EndPoints.BOARDS;
+import static com.epam.tc.api.hw3.utils.EndPoints.GET_BOARD;
 
 import com.epam.tc.api.hw3.dto.BoardDTO;
 import io.restassured.response.Response;
@@ -18,13 +19,15 @@ public class RestBoardService extends CommonService {
 
     public BoardDTO getBoard(String boardId) {
 
-        return getNoParams(String.format(BOARDS, boardId))
+        return getNoParams(String.format(GET_BOARD, boardId))
             .getBody().as(BoardDTO.class);
 
     }
 
     public Response deleteBoard(String boardId) {
 
-        return deleteNoParams(String.format(BOARDS, boardId));
+        return deleteNoParams(String.format(GET_BOARD, boardId));
     }
+
+
 }
--- src/main/java/com/epam/tc/api/hw3/utils/EndPoints.java
+++ src/main/java/com/epam/tc/api/hw3/utils/EndPoints.java
@@ -3,6 +3,7 @@ package com.epam.tc.api.hw3.utils;
 public final class EndPoints {
 
     public static final String BOARDS = "boards/";
+    public static final String GET_BOARD = "boards/%s";
     public static final String LISTS = "lists/";
     public static final String CARDS = "cards/";
     public static final String CHECKLISTS = "checklists/";
